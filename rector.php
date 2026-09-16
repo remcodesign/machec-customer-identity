8842e32c01aa53f52ed0aa69728c036936c659f5
Remco Jansen <remcojansendesign@gmail.com>
2026-09-16 18:03:58 +0200

    Refactor rector configuration paths and comment out two-factor authentication tests

--- rector.php
+++ rector.php
@@ -6,27 +6,19 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
+        __DIR__.'/app',
+        __DIR__.'/bootstrap',
+        __DIR__.'/config',
+        __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ])
     // uncomment to reach your current PHP version
     // ->withPhpSets()
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
-
-
-
-
-
-
-
-
 
 // <?php
 
@@ -61,4 +53,3 @@ return RectorConfig::configure()
 //     ->withRules([
 //         AddGenericReturnTypeToRelationsRector::class,
 //     ]);
-
