226a650a02694ba67d68ab6196ee5f3eb17e738f
Remco Jansen <remcojansendesign@gmail.com>
2026-09-16 18:13:03 +0200

    Refactor rate limiting and expectation extensions to use arrow functions for improved readability

--- rector.php
+++ rector.php
@@ -1,27 +1,5 @@
 <?php
 
-// declare(strict_types=1);
-
-// use Rector\Config\RectorConfig;
-
-// return RectorConfig::configure()
-//     ->withPaths([
-//         __DIR__.'/app',
-//         __DIR__.'/bootstrap',
-//         __DIR__.'/config',
-//         __DIR__.'/public',
-//         __DIR__.'/resources',
-//         __DIR__.'/routes',
-//         __DIR__.'/tests',
-//     ])
-//     // uncomment to reach your current PHP version
-//     ->withPhpSets()
-//     ->withTypeCoverageLevel(0)
-//     ->withDeadCodeLevel(0)
-//     ->withCodeQualityLevel(0);
-
-
-
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
@@ -30,9 +8,6 @@ use RectorLaravel\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector;
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
-        __DIR__.'/config',
-        __DIR__.'/public',
         __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/tests',
